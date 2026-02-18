0cd012225f648102c8f5b7a2ceafa5f5df5e090a
Jesse Sher <jebba272727@gmail.com>
2026-02-18 21:13:00 +1100

    added some features of the ai chat

--- script.js
+++ script.js
@@ -27,3 +27,23 @@ function openchat()
         f.classList.remove('open-chat');
     }
 }
+
+function send_ai()
+{
+    /*
+           <li class="ai-out"><p>Im bot</p></li>
+            <li class="usr-out"><p>Hello!</p></li>
+            <li class="ai-out"><p>what do u want!</p></li>
+    */
+    let usr_ai_chat_inp = document.getElementById('ai-chat-input');
+    let ai_chat_ul = document.getElementById('ai-chat-ul');
+
+    let new_usr_msg = document.createElement('li');
+    new_usr_msg.classList.add('usr-out');
+
+    let new_usr_msg_p = document.createElement('p');
+    new_usr_msg_p.innerText = usr_ai_chat_inp.value;
+    new_usr_msg.appendChild(new_usr_msg_p);
+
+    ai_chat_ul.appendChild(new_usr_msg)
+}
